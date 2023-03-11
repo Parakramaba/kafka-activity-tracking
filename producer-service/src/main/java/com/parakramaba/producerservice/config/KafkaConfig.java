b0816d468f8ef7c2af0351334dcfab4a6c22cc3c
Parakramaba <p.vitharana.pv@gmail.com>
2023-03-11 08:25:44 +0530

    [11] Prod : Remove commented code segments

--- producer-service/src/main/java/com/parakramaba/producerservice/config/KafkaConfig.java
+++ producer-service/src/main/java/com/parakramaba/producerservice/config/KafkaConfig.java
@@ -1,15 +1,9 @@
 package com.parakramaba.producerservice.config;
 
-import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.NewTopic;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.kafka.core.KafkaAdmin;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 public class KafkaConfig {
@@ -27,17 +21,5 @@ public class KafkaConfig {
                 .name("tracking.user-activity.update-product")
                 .build();
     }
-
-//    @Bean
-//    public ApplicationRunner runner(KafkaAdmin kafkaAdmin) {
-//        return args -> {
-//            AdminClient adminClient = AdminClient.create(kafkaAdmin.getConfigurationProperties());
-//            List<NewTopic> topics = new ArrayList<>();
-//            topics.add(TopicBuilder.name("tracking.user-activity.create-product").build());
-//            topics.add(TopicBuilder.name("tracking.user-activity.update-product").build());
-//            topics.add(TopicBuilder.name("tracking.user-activity.delete-product").build());
-//            adminClient.createTopics(topics).all().get();
-//        };
-//    }
 
 }
